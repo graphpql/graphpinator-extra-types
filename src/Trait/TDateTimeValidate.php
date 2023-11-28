6384da3f2ff3f14a73541105d99a233ffbc25d9e
Václav Pelíšek <peldax@gmail.com>
2023-11-28 13:07:59 +0100

    Relax date valdation little bit to allow timezone shortcut

--- src/Trait/TDateTimeValidate.php
+++ src/Trait/TDateTimeValidate.php
@@ -12,7 +12,6 @@ trait TDateTimeValidate
         $errors = \DateTimeImmutable::getLastErrors();
 
         return $dateTime instanceof \DateTime
-            && $dateTime->format($format) === $value
             && ($errors === false || ($errors['error_count'] === 0 && $errors['warning_count'] === 0));
     }
 }
